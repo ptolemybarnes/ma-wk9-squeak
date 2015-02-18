68fde85316736d7745a4913fa9e05410ccd1482e
Ptolemy <ptolemybarnes@gmail.com>
2015-02-18 17:07:44 +0000

    Create User model with signup/signin functionality, using devise.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'devise', '~> 3.4.1'
 
 gem 'haml', '~>4.0.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'restaurants' => 'restaurants#index'
   get 'new_restaurant' => 'restaurants#new'
+  root to: "restaurants#index"
 
   resources :restaurants do
     resources :reviews
