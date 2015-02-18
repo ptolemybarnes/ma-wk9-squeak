bfaac9f73db7f6696f785e0afda3c1f85c6b1632
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-18 11:15:42 +0000

    Added reviews to homepage

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,15 +4,13 @@ feature 'reviewing' do
   let!(:mamathai) {Restaurant.create name: 'Mama Thai' }
 
   scenario 'users can leave a review' do
-
-    mamathai
     visit '/restaurants'
     click_link 'Review Mama Thai'
     fill_in 'Thoughts', with: "Some things in life I will never get thai'd of..."
     select '3', from: 'Rating'
     click_button 'Squeak squeak!'
 
-    expect(current_path).to_eq('/restaurants')
+    expect(current_path).to eq('/restaurants')
     expect(page).to have_content("Some things in life I will never get thai'd of...")
   end
 end
