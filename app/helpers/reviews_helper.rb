7dc4ee885748ed940fe4fdc55e2318bb76de099c
Ptolemy <ptolemybarnes@gmail.com>
2015-02-23 09:28:55 +0000

    Pass acceptance test for star ratings.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,8 +1,8 @@
 module ReviewsHelper
   
   def star_rating(rating)
-    if rating.is_a? Integer
-      '★' * rating
+    if rating.is_a? Numeric
+      ('★' * rating.round) + ('☆' * (5 - rating.round))
     else
       'N/A'
     end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -11,6 +11,14 @@ describe ReviewsHelper, :type => :helper do
     it 'returns five black stars when passed 5' do
       expect(helper.star_rating(5)).to eq '★★★★★'
     end
+
+    it 'supplements black stars with white when passed less than 5' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
+    it 'returns 4 black and 1 white star when passed 3.5' do
+      expect(helper.star_rating(3.5)).to eq '★★★★☆'
+    end
   end
 end
       
